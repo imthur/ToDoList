42bada8e54c42437719e265cb76341af33927ba0
imthur <arthur.aguiar8708@gmail.com>
2025-03-25 16:58:10 -0300

    Primeiro commit - Adicionando arquivos

---
+++ ToDoList/mainApp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import *
+from .models import *
+
+#Operações CRUD
+
+class ListToDo(generics.ListAPIView): # List
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class DetailToDo(generics.RetrieveUpdateAPIView): # Detail
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class CreateToDo(generics.CreateAPIView): # Create
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+
+class DeleteToDo(generics.DestroyAPIView): # Delete
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
